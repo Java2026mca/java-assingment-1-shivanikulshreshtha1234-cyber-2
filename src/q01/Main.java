b7654ca9abd3a033ab850a3e4ddca37a607d996c
shivanikulshreshtha1234-cyber <shivanikulshreshtha1234@gmail.com>
2026-04-08 10:36:28 +0530

    Hollow Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -4,15 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // Upper part (including middle line)
         for (int i = 1; i <= n; i++) {
             // Print leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print stars and inner spaces
             for (int j = 1; j <= 2 * i - 1; j++) {
                 if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
@@ -24,14 +21,12 @@ public class Main {
             System.out.println();
         }
 
-        // Lower part
         for (int i = n - 1; i >= 1; i--) {
             // Print leading spaces
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            // Print stars and inner spaces
             for (int j = 1; j <= 2 * i - 1; j++) {
                 if (j == 1 || j == 2 * i - 1) {
                     System.out.print("*");
